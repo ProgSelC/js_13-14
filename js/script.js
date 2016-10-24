a75fee1df5b5e1bb38cce9ddabb13c38c40dca69
SelC <selc@mail.ru>
2016-10-25 00:05:59 +0300

    Done

--- js/script.js
+++ js/script.js
@@ -6,13 +6,14 @@ window.onbeforeunload = function () {
 
 $(function () {
     var quizz = {
-        "testName":"Тест пока не готов :(",
-        "submitText":"Проверить мои результаты",
+        "testName":"Moq test",
+        "submitText":"View results",
         "questions":[
             {
                 "id": "q1",
                 "answer": "100",
-                "text": "Are you programmer?",
+                "text": "Are you a programmer?",
+                "type": "radio",
                 "variants":[
                     { "text": "Sure!", "value": 1 },
                     { "text": "Of course no!", "value": 2 },
@@ -23,10 +24,23 @@ $(function () {
                 "id": "q2",
                 "answer": "10",
                 "text": "Which is not a programming language?",
+                "type": "radio",
                 "variants":[
                     { "text": "Pyton", "value": 1 },
                     { "text": "Sun", "value": 2 },
                     { "text": "JavaScript", "value": 4 }
+                ]
+            },
+            {
+                "id": "q3",
+                "answer": "1011",
+                "text": "Which results equal 0",
+                "type": "checkbox",
+                "variants":[
+                    { "text": " (1 ^ 1)", "value": 1 },
+                    { "text": " (1 ^ 1 & 1)", "value": 2 },
+                    { "text": " (0 | 1)", "value": 4 },
+                    { "text": " (1 ^ 0 ^ 1)", "value": 8 }
                 ]
             }
         ]
@@ -45,7 +59,7 @@ $(function () {
             var quizz = JSON.parse(localStorage.getItem('quizz'));
             return quizz;
         } catch(exception){
-            return { "testName":"Тест недоступен", "submitText":"Кнопку нажимать бессмысленно )", "questions": [] };
+            return { "testName":"The test is unreachable :(", "submitText":"No need to push the button", "questions": [] };
         }
     }
 
@@ -59,16 +73,21 @@ $(function () {
             $('#' + item.id + ' input:checked').each(function(index, el){
                 checkSum += parseInt($(el).val());
             });
-            
-            if ( checkSum^parseInt(item.answer, 2) == 0 ) {
+
+            if ( (checkSum^parseInt(item.answer, 2)) == 0 ) {
                 totalAnswered +=  1;
             }
         });
 
-        return "Правильных ответов " + totalAnswered + "/" + totalItems;
+        return (totalItems == totalAnswered) ? "Bingo!" : "Your result is: " + totalAnswered + "/" + totalItems;
     }
 
     $('button[type="submit"]').click(function() {
-        console.log(checkAnswers(savedQuizz));
+        $('#results .modal-body p').html(checkAnswers(savedQuizz));
+        $('#results').modal('show');
+    });
+    $('#results .btn-primary').click(function(){
+        $('input').prop('checked', false);
+        $('#results').modal('hide');
     });
 });
